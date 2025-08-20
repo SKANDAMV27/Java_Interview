b57a50e835febc2e0533a26a89117b92f2f59852
Skanda M V <skandagowda0@gmail.com>
2025-08-20 10:19:36 +0530

    Swap the Two Variable Without using Third Variable

---
+++ swap/src/com/xworkz/swap/Swap.java
@@ -0,0 +1,17 @@
+package com.xworkz.swap;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a=723, b=890;
+
+        System.out.println("Before Swap: "+"a="+a+" b="+b);
+
+        a= a+b;
+        b= a-b;
+        a= a-b;
+
+        System.out.println("After The Swap: "+"a="+a+" b="+b);
+
+        System.out.println("Swap Become Done");
+    }
+}
