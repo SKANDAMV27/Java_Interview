883fca0ea2aa07bdcf5f6e90460652d015bcd79a
Skanda M V <skandagowda0@gmail.com>
2025-08-22 03:00:11 +0530

    Palindrome

---
+++ xworkz/src/com/xworkz/interview/palindrom.java
@@ -0,0 +1,26 @@
+package com.xworkz.interview;
+
+import java.util.Scanner;
+
+public class palindrom {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter The Number: ");
+        int num = scanner.nextInt();
+
+        int temp = num; // convert temp to num
+        int reverse = 0; //
+
+        while (temp>0)
+        {
+            reverse = reverse*10+temp%10; //build the reverse number
+            temp/=10; //remove last digits
+        }
+        if(num==reverse){
+            System.out.println(num+"This Is Palindram");
+        }
+        else{
+            System.out.println(num+":This Is not a Palindrom");
+        }
+    }
+}
