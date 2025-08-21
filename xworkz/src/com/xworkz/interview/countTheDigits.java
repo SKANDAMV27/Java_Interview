3343de077934409f9ea2f7eb5228abe037fe0dd3
Skanda M V <skandagowda0@gmail.com>
2025-08-22 02:48:47 +0530

    Count The Digits

--- xworkz/src/com/xworkz/interview/countTheDigits.java
+++ xworkz/src/com/xworkz/interview/countTheDigits.java
@@ -1,5 +1,26 @@
 package com.xworkz.interview;
 
+import com.sun.javafx.css.Size;
+
+import java.util.Scanner;
+
 public class countTheDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter The Number's To Count: ");
+        int num = scanner.nextInt();
+
+        int sum = 0; //sum from 0
+
+        int temp = num;// then convert number to temp
+
+        while (temp>0)//loop until temp become 0
+        {
+            sum+= temp%10; // Take Last Digits and all it to sum
+            temp/=10; // Remove The Last digits
+        }
+
+        System.out.println("Then"+num+":"+sum);
+    }
 
 }
