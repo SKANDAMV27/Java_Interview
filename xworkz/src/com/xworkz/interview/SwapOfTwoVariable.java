ebed9b72f54078f0ff987029a8cf7928d0830c00
Skanda M V <skandagowda0@gmail.com>
2025-08-21 10:39:24 +0530

    Swap of two variable without using the third Variable

---
+++ xworkz/src/com/xworkz/interview/SwapOfTwoVariable.java
@@ -0,0 +1,20 @@
+package com.xworkz.interview;
+
+public class SwapOfTwoVariable {
+    public static void main(String[] args) {
+        int a=250, b=500;
+        System.out.println("Before Swap");
+        System.out.println("a="+a+"b="+b);
+
+        a= a+b;
+        b= a-b;
+        a=a-b;
+
+        System.out.println("...After The Swap...");
+        System.out.print("a="+a+"b="+b);
+
+        System.out.println("\nSwap Between Two Number");
+
+
+    }
+}
