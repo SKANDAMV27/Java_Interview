c93188eacd560a4fad8d1d8d23d8f24eddac9799
Skanda M V <skandagowda0@gmail.com>
2025-08-22 03:13:20 +0530

    Reverse The Digits

---
+++ xworkz/src/com/xworkz/interview/reverseNumber.java
@@ -0,0 +1,22 @@
+package com.xworkz.interview;
+
+import java.util.Scanner;
+
+public class reverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter The Number To Reverse: ");
+        int num= scanner.nextInt();
+
+        int temp = num;
+        int reverse = 0;
+
+        while (temp>0){
+            reverse = reverse*10+temp%10; //build the reverse number
+            temp/=10;//remove the last digits
+        }
+        System.out.println("After The Reverse: "+reverse);
+
+
+    }
+}
