869f128150611dd60ed00f6c44aa6f527e9387f9
Skanda M V <skandagowda0@gmail.com>
2025-08-22 23:16:18 +0530

    Leap Year or Not

---
+++ xworkz/src/com/xworkz/interview/LeapYear.java
@@ -0,0 +1,20 @@
+package com.xworkz.interview;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter The Year: ");
+        int year = scanner.nextInt();
+
+        if((year%4==0 && year%100!=0)||(year%400==0)){
+            System.out.println(year+" :Leap Year");
+        }
+            else{
+            System.out.println(year+":Not a Leap Year");
+            }
+
+        }
+    }
+
