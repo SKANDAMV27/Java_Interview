873f375eb0d34db1efb8ed5c4552302211d082d9
Skanda M V <skandagowda0@gmail.com>
2025-08-28 09:13:12 +0530

    Swap Two Variable

--- xworkz/src/com/xworkz/interview/SwapOfTwoVariable.java
+++ xworkz/src/com/xworkz/interview/SwapOfTwoVariable.java
@@ -1,10 +1,23 @@
 package com.xworkz.interview;
 
+import java.util.Scanner;
+
 public class SwapOfTwoVariable {
     public static void main(String[] args) {
-        int a=250, b=500;
-        System.out.println("Before Swap");
-        System.out.println("a="+a+"b="+b);
+//        int a=250, b=500;
+//        System.out.println("Before Swap");
+//        System.out.println("a="+a+"b="+b);
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        int b;
+        System.out.println("Enter The Value To a: ");
+        int num = scanner.nextInt();
+
+        System.out.println("Enter The Value To b: ");
+        int num1= scanner.nextInt();
+
+        a = num;
+        b=num1;
 
         a= a+b;
         b= a-b;
