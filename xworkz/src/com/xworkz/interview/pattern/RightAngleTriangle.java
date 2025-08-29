94278530b6288847397bef510a7be89a5456cf00
Skanda M V <skandagowda0@gmail.com>
2025-08-29 09:14:24 +0530

    Right Angle Triangle

---
+++ xworkz/src/com/xworkz/interview/pattern/RightAngleTriangle.java
@@ -0,0 +1,16 @@
+package com.xworkz.interview.pattern;
+
+public class RightAngleTriangle {
+    public static void main(String[] args) {
+        int i;
+        int j;
+//        int n;
+        for(i=1;i<=5;i++){
+            for(j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+}
