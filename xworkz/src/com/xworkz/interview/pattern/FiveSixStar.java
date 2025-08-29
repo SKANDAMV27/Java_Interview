6d0edb7ef5398534503d585df1b9eed08823664f
Skanda M V <skandagowda0@gmail.com>
2025-08-29 08:59:29 +0530

    Print Five Star In  Row and Six Star In column

---
+++ xworkz/src/com/xworkz/interview/pattern/FiveSixStar.java
@@ -0,0 +1,14 @@
+package com.xworkz.interview.pattern;
+
+public class FiveSixStar {
+    public static void main(String[] args) {
+        int i;
+        int j;
+        for(i=1;i<=5;i++){
+            for(j=1;j<=6;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
